2efd12522102210297f6a9b7b9a479adc067490e
Annabelle Strong <annabellestrong@gmail.com>
2022-02-06 13:53:40 -0500

    more deployment

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # navigate into the build output directory
-#cd dist
+cd build
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:anstrong/anstrong.github.io.git master
+#git push -f git@github.com:anstrong/anstrong.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
-#cd -
+cd -
