88f2b5119f78623c1b7bfcbe7b1f54986fbdf4db
Annabelle Strong <annabellestrong@gmail.com>
2022-02-06 15:19:41 -0500

    even more deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  build: {
+  /*build: {
     outDir: "build"
   },
   server: {
@@ -12,5 +12,5 @@ export default defineConfig({
     hmr: {
       port: 443 // Run the websocket server on the SSL port
     }
-  }
+  }*/
 });
