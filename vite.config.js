d3ca0dc1b056cbb0a2f9bcfa2c45ae624307ca00
Annabelle Strong <annabellestrong@gmail.com>
2022-02-07 13:00:11 -0500

    Bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-  /*build: {
+  plugins: [react()],
+  build: {
     outDir: "build"
-  },
+  }/*,
   server: {
     strictPort: true,
     hmr: {
