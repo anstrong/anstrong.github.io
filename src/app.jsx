6fd6bfadd965f08a530647404dd7ba8e13a52a94
Annabelle Strong <annabellestrong@gmail.com>
2022-04-05 16:13:50 -0400

    mobile support

--- src/app.jsx
+++ src/app.jsx
@@ -2,44 +2,84 @@ import React, { useState, useEffect } from "react";
 import { Router, Link } from "wouter";
 import "./styles/styles.css";
 import PageRouter from "./components/router.jsx";
+import About from "./pages/about";
 import useHashLocation from "./hooks/wouter-hash";
 import Seo from './components/seo.jsx';
 import SocialButtonSet from './components/social.jsx';
 
 // Home function that is reflected across the site
+const useMediaQuery = (query, whenTrue, whenFalse) => {
+  if (typeof window === 'undefined' || typeof window.matchMedia === 'undefined')
+    return whenFalse;
+
+  const mediaQuery = window.matchMedia(query);
+  const [match, setMatch] = React.useState(!!mediaQuery.matches);
+
+  React.useEffect(() => {
+    const handler = () => setMatch(!!mediaQuery.matches);
+    mediaQuery.addListener(handler);
+    return () => mediaQuery.removeListener(handler);
+  }, []);
+
+  return match ? whenTrue : whenFalse;
+};
 export default function Home() {
   const buttonList = [{
-                        label: "LinkedIn",
-                        icon: "fa-linkedin-in",
-                        address: "https://www.linkedin.com/in/annabellestrong/"
-                      },
-                      {
-                        label: "GitHub",
-                        icon: "fa-github",
-                        address: "https://github.com/anstrong"
-                      }
-                    ]
-  return (
-    <Router>
-      <Seo />
-      <main role="main" className="wrapper">
-        <div className="links top-bar">
-          <Link href="/"><i class="fas fa-home back"></i></Link>
-        </div>
-        <div className="content">
-          <PageRouter />
-        </div>
-      </main>
-      <footer className="footer fixed-bottom">
-        <div className="links">
-          <SocialButtonSet
-            buttons={buttonList}
-          />
-        </div>
-        <div>
-          <a href="https://github.com/anstrong/anstrong.github.io/blob/9161a71522d17336688c04306876db832a1ad1d6/assets/Resume.pdf" className="download-button" title="View PDF Resume"><i className="fas fa-file-download fa-2x download-icon"></i></a>
-        </div>
-      </footer>
-    </Router>
-  );
+    label: "LinkedIn",
+    icon: "fa-linkedin-in",
+    address: "https://www.linkedin.com/in/annabellestrong/"
+  },
+  {
+    label: "GitHub",
+    icon: "fa-github",
+    address: "https://github.com/anstrong"
+  }
+  ]
+  const isMobile = useMediaQuery({ query: '(max-width: 1000px)' });
+  if(isMobile) {
+    return (
+      <Switch>
+        <Route path="/" component={
+          <main role="main" className="wrapper">
+          <div className="links top-bar">
+            <SocialButtonSet
+              buttons={buttonList}
+            />
+          </div>
+          <div className="content">
+          <About />
+          <div>
+            <a href="https://github.com/anstrong/anstrong.github.io/blob/9161a71522d17336688c04306876db832a1ad1d6/assets/Resume.pdf" className="download-button" title="View PDF Resume"><i className="fas fa-file-download fa-2x download-icon"></i></a>
+          </div>
+          </div>
+          </main>
+        } />
+      </Switch>
+    );
+  }
+  else {
+    return (
+      <Router>
+        <Seo />
+        <main role="main" className="wrapper">
+          <div className="links top-bar">
+            <Link href="/"><i class="fas fa-home back"></i></Link>
+          </div>
+          <div className="content">
+            <PageRouter />
+          </div>
+        </main>
+        <footer className="footer fixed-bottom">
+          <div className="links">
+            <SocialButtonSet
+              buttons={buttonList}
+            />
+          </div>
+          <div>
+            <a href="https://github.com/anstrong/anstrong.github.io/blob/9161a71522d17336688c04306876db832a1ad1d6/assets/Resume.pdf" className="download-button" title="View PDF Resume"><i className="fas fa-file-download fa-2x download-icon"></i></a>
+          </div>
+        </footer>
+      </Router>
+    );
+  }
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,10 +7,11 @@ import Menu from "../components/menu.jsx";
 const imgs = ["https://cdn.glitch.me/47315a0e-1265-4e95-aa84-887a21ec6fb2%2Flogo.png?v=1635296262360",]
 var currentImg = 0;
 
+
 export default function Home() {
   var data = myData;
-  var pages=["about", "resume","projects"];
-  
+  var pages = ["about", "resume", "projects"];
+
   return (
     <div className = "content">
       <img class="profile" src={imgs[currentImg]}></img>
