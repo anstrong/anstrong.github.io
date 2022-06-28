3d441bd5d07d43b046f744dd6f9f8dd768d9dfa4
Annabelle Strong <annabellestrong@gmail.com>
2022-06-28 13:55:52 -0400

    Update link.jsx

--- src/components/link.jsx
+++ src/components/link.jsx
@@ -14,15 +14,16 @@ const Button = props => (
     <a href={props.address} target="_blank" title={props.label}><i className={`sm_icon ${props.icon}`}></i></a>
 )
 
-const File = address => (
-    new URL(`../../public/files/${address}`, import.meta.url)
-)
+const File = address => {
+    return new URL(`/files/${address}`, import.meta.url);
+}
 
 const LinkedAsset = props => {
+    console.log(import.meta.url);
     if (props && props.address) {
         return (
             <Button
-                address={!props.address.includes("http") ? File(props.address).href : props.address}
+                address={!props.address.includes("http") ? File(props.address).href: props.address}
                 icon={!props.icon || !props.icon.includes("-") ? icons[props.label] : props.icon}
                 label={props.label}
                 newTab={props.address.includes("http")}
